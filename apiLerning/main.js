ce3a2928881ec5a9df6b9d027e8ed5b6f2d0e44d
T0karevS <90570998+T0karevS@users.noreply.github.com>
2022-05-05 23:56:03 +0400

    Add files via upload

--- apiLerning/main.js
+++ apiLerning/main.js
@@ -1,3 +1,4 @@
+let id = null;
 async function getPosts(){
     let res = await fetch('http://rp-32/apishka/api1/posts');
     let posts = await res.json();
@@ -8,8 +9,8 @@ async function getPosts(){
             <div class="card-body">
                 <h5 class="card-title">${post.title}</h5>
                 <p class="card-text">${post.body}</p>
-                <a href="#" class="card-link" onclick="removePost(${post.id})">Удалиить</a>
-                <a href="#" class="card-link" onclick="selectPost()">Редактировать</a>
+                <a href="#" class="card-link" onclick="removePost(${post.id})">Удалить</a>
+                <a href="#" class="card-link" onclick="selectPost(${post.id}, ${post.title}, ${post.body})">Редактировать</a>
             </div>
         </div>        
         `
@@ -41,6 +42,33 @@ async function removePost(id)
     });
     const data = await res.json();
     if(data.status === true)
+    {
+        await getPosts();
+    }
+}
+function selectPost(ids, title, body){
+    id = ids;
+    document.getElementById('titleedit1').value = {title};
+    document.getElementById('bodyedit1').value = body;
+    
+}
+async function updatePost()
+{
+    const title = document.getElementById('titleedit1').value,
+    body = document.getElementById('bodyedit1').value;
+
+    const data = {
+        title: title,
+        body: body
+    }
+
+    const res = await fetch(`http://rp-32/apishka/api1/posts/${id}`,{
+        method: 'PATCH',
+        body: JSON.stringify(data)
+    })
+
+    let resData = await res.json();
+    if(resData.status === true)
     {
         await getPosts();
     }
